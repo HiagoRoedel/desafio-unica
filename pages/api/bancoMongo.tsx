c50e9fe76d55cf242dd13412284076cafc335d5e
Hiago <hiagojunio4@gmail.com>
2024-06-13 17:36:11 -0300

    comment banco mongodb

--- pages/api/bancoMongo.tsx
+++ pages/api/bancoMongo.tsx
@@ -1,14 +1,14 @@
-import connect from "../../util/database2"
+// import connect from "../../util/database2"
 
-export default async function handler(req, res){
+// export default async function handler(req, res){
  
-    const {db} = await connect()
+//     const {db} = await connect()
 
-    const user = {
-        modality: "pos-graduacao"
-    }
+//     const user = {
+//         modality: "pos-graduacao"
+//     }
 
-    const response = await db.collection("depoiments").findOne(user)
+//     const response = await db.collection("depoiments").findOne(user)
 
-    res.status(200).json(response)
-}
+//     res.status(200).json(response)
+// }
