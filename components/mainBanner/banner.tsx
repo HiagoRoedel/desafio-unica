0ffbd73471bfc707039207f25b6774f058421f11
Hiago <hiagojunio4@gmail.com>
2024-06-14 15:40:47 -0300

    website finished about the unique challenge --- prominas

--- components/mainBanner/banner.tsx
+++ components/mainBanner/banner.tsx
@@ -1,16 +1,18 @@
+
 import './banner.css'
 
 export default function Banner() {
     return (
-        <>
-            <section className="main-banner-pos-graduacao banner-top-section imagem">
+        <> 
+        <section className="main-banner-pos-graduacao banner-top-section imagem"> 
                 <div className="container mx-auto titlemainbanner">
                     <span className="category-title">
                         CONHEÇA A
                     </span>
                     <h1>FACULDADE ÚNICA</h1>
                 </div>
-            </section>
+        </section>
+
         </>
     )
 }
